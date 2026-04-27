92c49c4246754319b9dd96df733f3b4bfbee5009
Andre Waleczki <waleczki.andre@outlook.com>
2026-04-27 14:41:02 -0300

    fix(ec2): fix docker shell replacement syntax

--- terraform/modules/airflow_ec2/main.tf
+++ terraform/modules/airflow_ec2/main.tf
@@ -63,7 +63,7 @@ resource "aws_instance" "airflow" {
 apt-get update -y
 apt-get install -y apt-transport-https ca-certificates curl software-properties-common python3-pip python3-venv git
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $$(lsb_release -cs) stable"
+add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update -y
 apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 usermod -aG docker ubuntu
